80d1bf9b570500f0c26ec7a185761358bd8c880f
Evan Plaice <evanplaice@gmail.com>
2020-02-11 00:13:01 -0700

    Add Build

---
+++ index.cjs
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * MockConsole class used to manage the 'console' built-in methods (ex log, info, error)
+ */
+class MockConsole {
+  constructor() {
+    const proto = Object.getPrototypeOf(this);
+    if(!proto.instance){
+      this.log = console.log;
+      this.info = console.info;
+      this.error = console.error;
+      this.logs = [];
+      this.infos = [];
+      this.errors = [];
+      proto.instance = this;
+    }
+    return proto.instance;
+  }
+
+  /**
+   * Disable the console built-in methods
+   */
+  disable () {
+    console.log = () => {};
+    console.info = () => {};
+    console.error = () => {};
+    this.flush();
+  }
+
+  /**
+   * Capture the output of the console built-ins
+   */
+  capture () {
+    console.log = log => { this.logs.push(log); };
+    console.info = info => { this.infos.push(info); };
+    console.error = err => { this.errors.push(err); };
+  }
+
+  /**
+   * Restore the built-in console methods
+   */
+  restore () {
+    console.log = this.log;
+    console.info = this.info;
+    console.error = this.error;
+  }
+
+  /**
+   * Flush the captured console output
+   */
+  flush () {
+    this.logs = [];
+    this.infos = [];
+    this.errors = [];
+  }
+}
+
+module.exports = MockConsole;
---
+++ index.d.ts
@@ -0,0 +1,27 @@
+/**
+ * MockConsole class used to manage the 'console' built-in methods (ex log, info, error)
+ */
+export default class MockConsole {
+    log: (message?: any, ...optionalParams: any[]) => void;
+    info: (message?: any, ...optionalParams: any[]) => void;
+    error: (message?: any, ...optionalParams: any[]) => void;
+    logs: any[];
+    infos: any[];
+    errors: any[];
+    /**
+     * Disable the console built-in methods
+     */
+    disable(): void;
+    /**
+     * Capture the output of the console built-ins
+     */
+    capture(): void;
+    /**
+     * Restore the built-in console methods
+     */
+    restore(): void;
+    /**
+     * Flush the captured console output
+     */
+    flush(): void;
+}
